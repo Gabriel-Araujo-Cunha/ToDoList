5b52c912383274709246ee2f0aef5e03ab7a7600
Gabriel-Araujo-Cunha <gabriel.araujo.cunha.2001@gmail.com>
2025-09-07 19:31:28 -0300

    estrutura ToDo

---
+++ src/components/Button.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Button = () => {
+  return (
+    <div>
+      <button>Completar</button>
+      <button>X</button>
+    </div>
+  );
+};
+
+export default Button;
