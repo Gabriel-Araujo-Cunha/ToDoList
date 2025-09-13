2b1de7dbd0560226f0630397e911bc9a4166e2bd
Gabriel-Araujo-Cunha <gabriel.araujo.cunha.2001@gmail.com>
2025-09-13 17:03:51 -0300

    filter | search

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
+
 import Todo from "./components/Todo";
 import TodoForm from "./components/TodoForm";
+import Search from "./components/search";
+import Filter from "./components/Filter";
 
 import "./App.css";
 
@@ -26,6 +29,11 @@ function App() {
     },
   ]);
 
+  const [search, setSearch] = useState("");
+
+  const [filter, setFilter] = useState("All");
+  const [sort, setSort] = useState("Asc");
+
   const addTodo = (text, category) => {
     const newTodo = [
       ...todos,
@@ -41,24 +49,51 @@ function App() {
   };
 
   const removedTodo = (id) => {
-    const newTodos = [...todos]
-    const filteredTodos = newTodos.filter(todo => todo.id !== id ? todo : null);
+    const newTodos = [...todos];
+    const filteredTodos = newTodos.filter((todo) =>
+      todo.id !== id ? todo : null
+    );
     setTodos(filteredTodos);
   };
 
   const completeTodo = (id) => {
     const newTodos = [...todos];
-    newTodos.map((todo) => todo.id === id ? todo.isCompleted = !todo.isCompleted: todo);
+    newTodos.map((todo) =>
+      todo.id === id ? (todo.isCompleted = !todo.isCompleted) : todo
+    );
     setTodos(newTodos);
-  }
+  };
 
   return (
     <div className="app">
       <h1>Lista de Tarefas</h1>
+      <Search search={search} setSearch={setSearch} />
+      <Filter filter={filter} setFilter={setFilter} setSort={setSort} />
       <div className="todo-list">
-        {todos.map((todo) => (
-          <Todo key={todo.id} todo={todo} removedTodo={removedTodo} completeTodo = {completeTodo} />
-        ))}
+        {todos
+          .filter((todo) =>
+            filter === "All"
+              ? true
+              : filter === "Completed"
+              ? todo.isCompleted
+              : !todo.isCompleted
+          )
+          .filter((todo) =>
+            todo.text.toLowerCase().includes(search.toLowerCase())
+          )
+          .sort((a, b) =>
+            sort === "Asc"
+              ? a.text.localeCompare(b.text)
+              : b.text.localeCompare(a.text)
+          )
+          .map((todo) => (
+            <Todo
+              key={todo.id}
+              todo={todo}
+              removedTodo={removedTodo}
+              completeTodo={completeTodo}
+            />
+          ))}
       </div>
       <TodoForm addTodo={addTodo} />
     </div>
